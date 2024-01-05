90c559799c9e7e3a0abdf6003d15ec9a4d230ed4
Sinux <aung.atg1738@gmail.com>
2024-01-05 21:26:58 +0630

    IThinkIt'sFinish

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <section>
+      <div class="loader"></div>
+    </section>
+  );
+};
+
+export default Loader;
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,7 +1,25 @@
 import { redirect } from "react-router-dom";
 
+export const getExpDuration = () => {
+  const expDate = localStorage.getItem("exp");
+  const expDateInMili = new Date(expDate);
+  const currentDateInMili = new Date();
+  const duration = expDateInMili - currentDateInMili;
+
+  return duration;
+};
+
 export const getToken = () => {
   const token = localStorage.getItem("token");
+  if (!token) {
+    return null;
+  }
+
+  const duration = getExpDuration();
+  if (duration < 0) {
+    return "TOKEN EXP";
+  }
+
   return token;
 };
 
@@ -15,4 +33,6 @@ export const checkTokenLoader = () => {
   if (!token) {
     return redirect("/auth?mode=login");
   }
+
+  return token;
 };
